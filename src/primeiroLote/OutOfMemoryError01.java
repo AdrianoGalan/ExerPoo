20c965397a08380b09e6362e06c8a6b7c3d60872
adriano <adriano@dell-xubuntu>
2020-10-16 00:50:52 -0300

    Exercicio - java.lang.OutOfMemoryError: Java heap space Crie um programa que estoure a memória Heap do Java para que ocorra o erro :
    java.lang.OutOfMemoryError: Java heap spac

---
+++ src/primeiroLote/OutOfMemoryError01.java
@@ -0,0 +1,43 @@
+package primeiroLote;
+
+import java.util.ArrayList;
+
+public class OutOfMemoryError01 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Programa 01 ");
+
+		ArrayList<Pessoa> pessoas = new ArrayList<>();
+
+		for (int i = 0; i < 100000000; i++) {
+
+			Pessoa pessoa = new Pessoa("" + i, i);
+
+			pessoas.add(pessoa);
+
+			System.out.println(pessoas.get(i));
+
+		}
+
+	}
+
+	static class Pessoa {
+
+		String nome;
+		int idade;
+
+		public Pessoa(String nome, int idade) {
+			super();
+			this.nome = nome;
+			this.idade = idade;
+		}
+
+		@Override
+		public String toString() {
+			return "Pessoa [nome=" + nome + ", idade=" + idade + "]";
+		}
+
+	}
+
+}
