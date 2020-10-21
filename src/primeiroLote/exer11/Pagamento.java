de7b1194d5be806f719b6e5d90c8fe70da2ffb0e
AdrianoGalan <dricogalan@gmail.com>
2020-10-21 14:38:39 -0300

    formaPagamento classe interna

--- src/primeiroLote/exer11/Pagamento.java
+++ src/primeiroLote/exer11/Pagamento.java
@@ -8,45 +8,81 @@ public class Pagamento {
 
 	public static void main(String[] args) {
 
-		Map<String, String> tipoPagamento = new HashMap<String, String>();
+		Map<Character, FormaPagamento> tipoPagamento = new HashMap<Character, FormaPagamento>();
 		Scanner scan = new Scanner(System.in);
 		String entrada = "";
+		Character chave = new Character('r');
 		double valor = 0;
 
-		tipoPagamento.put("Dinheiro", " em dinheiro");
-		tipoPagamento.put("Cartão Debito", " em cartão de débito");
-		tipoPagamento.put("Cartão de Crédito", " do limite do cartão de crédito");
-		tipoPagamento.put("Cheque", " em cheque");
-		tipoPagamento.put("Depósito", " em deposito bancario");
+		FormaPagamento fp1 = new FormaPagamento("Dinheiro", " em dinheiro");
+		FormaPagamento fp2 = new FormaPagamento("Cartão Debito", " em cartão de débito");
+		FormaPagamento fp3 = new FormaPagamento("Cartão de Crédito", " do limite do cartão de crédito");
+		FormaPagamento fp4 = new FormaPagamento("Cheque", " em cheque");
+		FormaPagamento fp5 = new FormaPagamento("Depósito", " em deposito bancario");
+
+		tipoPagamento.put('a', fp1);
+		tipoPagamento.put('b', fp2);
+		tipoPagamento.put('c', fp3);
+		tipoPagamento.put('d', fp4);
+		tipoPagamento.put('e', fp5);
 
 		do {
 			System.out.println("Digite o valor ");
 			valor = lerDouble(scan);
 			System.out.println("Digite a forma de pagamento ");
-			entrada = scan.nextLine();
-			if (tipoPagamento.containsKey(entrada)) {
-				System.out.println("Pago R$: " + valor + tipoPagamento.get(entrada));
+			chave = scan.nextLine().charAt(0);
+			if (tipoPagamento.containsKey(chave)) {
+				System.out.println("Pago R$: " + valor + tipoPagamento.get(chave).getDescricao());
 			} else {
 				System.out.println("Tipo de pagamento invalido: ");
 			}
-			
+
 			System.out.println("Digite s-sair / c-continuar");
 			entrada = scan.nextLine();
-			
+
 		} while (!entrada.equals("s"));
-		
+
 		System.out.println("saindo ....");
 		System.exit(0);
 
 	}
-	
+
 	private static double lerDouble(Scanner scan) {
-		
+
 		String digitado = "";
 
 		digitado = scan.nextLine();
 
 		return Double.parseDouble(digitado);
+	}
+
+	static class FormaPagamento {
+
+		private String tipo;
+		private String descricao;
+
+		public FormaPagamento(String tipo, String descricao) {
+
+			this.tipo = tipo;
+			this.descricao = descricao;
+		}
+
+		public String getTipo() {
+			return tipo;
+		}
+
+		public void setTipo(String tipo) {
+			this.tipo = tipo;
+		}
+
+		public String getDescricao() {
+			return descricao;
+		}
+
+		public void setDescricao(String descricao) {
+			this.descricao = descricao;
+		}
+
 	}
 
 }
